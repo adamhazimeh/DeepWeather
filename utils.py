7e985c56da8abe5562849f846fd31fce6095e6ab
Adam Hazimeh <86377382+adamhazimeh@users.noreply.github.com>
2021-12-27 03:28:39 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,18 @@
+"""
+Utilities used by example notebooks
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_image(image, factor=1.0, clip_range=None, **kwargs):
+    """
+    Utility function for plotting RGB images.
+    """
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
+    if clip_range is not None:
+        ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
+    else:
+        ax.imshow(image * factor, **kwargs)
+    ax.set_xticks([])
+    ax.set_yticks([])
